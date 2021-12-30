7111860fe62948193c56c51d33ab77a2c4f1e1f1
Shai Yehezkel <shaiyehezkel@campus.technion.ac.il>
2021-12-30 21:00:20 +0200

    Initial commit

---
+++ hw3/answers.py
@@ -0,0 +1,199 @@
+r"""
+Use this module to write your answers to the questions in the notebook.
+
+Note: Inside the answer strings you can use Markdown format and also LaTeX
+math (delimited with $$).
+"""
+
+# ==============
+# Part 1 answers
+
+part1_q1 = r"""
+**Your answer:**
+Defining Z to be the output of the layer, we'll get a matrix size of batch_size X out_feature since this is a FC layer.
+Therefore $shape{Z} =(128,2048)$
+Defining X the input matrix which we'll calculate the jacobian by is size 128 X in_features.
+Therefore $shape{X} =(128,1024)$
+
+The jacobian tensor with respect to X will be a 4D tensor of the following  dimensions:
+
+$(128,1024,128,2048)$
+
+"""
+
+part1_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+# ==============
+# Part 2 answers
+
+
+def part2_overfit_hp():
+    wstd, lr, reg = 0, 0, 0
+    # TODO: Tweak the hyperparameters until you overfit the small dataset.
+    # ====== YOUR CODE: ======
+    wstd, lr, reg = 0.09, 0.05, 0.03
+    # ========================
+    return dict(wstd=wstd, lr=lr, reg=reg)
+
+
+def part2_optim_hp():
+    wstd, lr_vanilla, lr_momentum, lr_rmsprop, reg, = 0, 0, 0, 0, 0
+
+    # TODO: Tweak the hyperparameters to get the best results you can.
+    # You may want to use different learning rates for each optimizer.
+    # ====== YOUR CODE: ======
+    wstd, lr_vanilla, lr_momentum, lr_rmsprop, reg = 1e-1, 1.5e-2, 3e-3, 1.8e-4, 1e-2
+    # ========================
+    return dict(
+        wstd=wstd,
+        lr_vanilla=lr_vanilla,
+        lr_momentum=lr_momentum,
+        lr_rmsprop=lr_rmsprop,
+        reg=reg,
+    )
+
+
+def part2_dropout_hp():
+    wstd, lr, = 0, 0
+    # TODO: Tweak the hyperparameters to get the model to overfit without
+    # dropout.
+    # ====== YOUR CODE: ======
+    raise NotImplementedError()
+    # ========================
+    return dict(wstd=wstd, lr=lr)
+
+
+part2_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part2_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part2_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+# ==============
+
+# ==============
+# Part 3 answers
+
+
+def part3_rnn_hyperparams():
+    hypers = dict(
+        batch_size=0,
+        seq_len=0,
+        h_dim=0,
+        n_layers=0,
+        dropout=0,
+        learn_rate=0.0,
+        lr_sched_factor=0.0,
+        lr_sched_patience=0,
+    )
+    # TODO: Set the hyperparameters to train the model.
+    # ====== YOUR CODE: ======
+    raise NotImplementedError()
+    # ========================
+    return hypers
+
+
+def part3_generation_params():
+    start_seq = ""
+    temperature = 0.0001
+    # TODO: Tweak the parameters to generate a literary masterpiece.
+    # ====== YOUR CODE: ======
+    raise NotImplementedError()
+    # ========================
+    return start_seq, temperature
+
+
+part3_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q4 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+# ==============
